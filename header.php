257748f0ce49a7da1ba89cd21c46f38364ec4705
manimac <manimaccse@gmail.com>
2019-05-14 10:15:18 +0530

    php basics,validations,form handling

---
+++ header.php
@@ -0,0 +1,2 @@
+<a href="index.php">Home</a>
+<a href="about.php">About</a>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+ include('header.php'); 
+    echo "<h1><b><u>haisd</u></b></h1>";
+    // echo "<script>alert('test')</script>";
+    $name = 'Dinesh';
+    $val = 10;
+    echo $name ."<br>";
+    echo $val."<br>";
+    $avalue = 20;
+    $bvalue = 20;
+    if($avalue < $bvalue){
+        echo "B is greater than A";
+    }
+    elseif($avalue > $bvalue){
+        echo "A is greater than B";
+    }
+    else{
+        echo "Both are equal";
+    }
+
+    if(isset($_POST['save'])){
+        $avalue = $_POST['a'];
+        $bvalue = $_POST['b'];
+        if(!$avalue){
+            echo "<script>alert('Please enter a value')</script>";
+        }
+        else if(!$bvalue){
+            echo "<script>alert('Please enter b value')</script>";
+        }
+        else{
+            if($avalue < $bvalue){
+                echo "B is greater than A";
+            }
+            elseif($avalue > $bvalue){
+                echo "A is greater than B";
+            }
+            else{
+                echo "Both are equal";
+            }
+        }        
+    }
+?>
+
+<html>
+    <head>
+    </head>
+    <body>
+    
+    <form method="post">
+        <input type="text" name="a">
+        <input type="text" name="b">
+        <input type="submit" name="save" value="Save">
+    </form>
+    
+    </body>
+</html>
